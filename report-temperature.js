80deca1f6ff3b924bd2f0d02f88565994995cd90
Alex Vanderbist <alex.vanderbist@gmail.com>
2018-10-07 16:10:12 +0200

    Init

---
+++ report-temperature.js
@@ -0,0 +1,48 @@
+const raspi = require('raspi');
+const OneWire = require('raspi-onewire').OneWire;
+const request = require('request');
+require('dotenv').config();
+
+raspi.init(() => {
+    const bus = new OneWire();
+    bus.searchForDevices((err, devices) => {
+        bus.readAllAvailable(devices[0], (err, data) => {
+            parseData(data.toString());
+        });
+    });
+});
+
+function parseData(data)
+{
+    let temperature = Number(data.match(/t=(\d+)/)[1]);
+
+    if (temperature === 0) {
+        new Error(`Invalid sensor data: ${data}`);
+    }
+
+    temperature = temperature / 1000;
+
+    console.log(`temperature=${temperature}`);
+
+    reportTemperature(temperature);
+}
+
+function reportTemperature(temperature)
+{
+    const username = process.env.DASHBOARD_USERNAME;
+    const password = process.env.DASHBOARD_PASSWORD;
+    const url = process.env.DASHBOARD_URL + '/temperature';
+    const auth = 'Basic ' + new Buffer(`${username}:${password}`).toString("base64");
+
+    request.post({
+            url,
+            headers: { "Authorization": auth },
+            json: { temperature },
+        },
+        error => {
+            if (error) {
+                console.error(error);
+            }
+        }
+    );
+}
